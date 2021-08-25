c30300d3628aa5448afd57a41c009cad6064ec56
ahui <619899573@qq.com>
2021-08-25 14:50:13 +0800

    update

--- transform/yoloAnchor.py
+++ transform/yoloAnchor.py
@@ -10,9 +10,9 @@ YOLO 格式的数据集转化为 COCO 格式的数据集
 --random_split 为划分参数，如果没有这个参数则只保存train.json文件
 
 命令行：
-python yoloAnchor.py --root_dir ../..
-python yoloAnchor.py --root_dir ../../dataset_test/test_merge_f --random_split
-python yoloAnchor.py --root_dir ./train --random_split
+python yoloAnchor.py --root_dir ../../merge_full
+
+--random_split
 """
 import os
 import argparse
@@ -22,57 +22,67 @@ from scipy.cluster.vq import kmeans
 parser = argparse.ArgumentParser()
 parser.add_argument('--root_dir', default='./data', type=str,
                     help="root path of images and labels, include ./images and ./labels and classes.txt")
-parser.add_argument('--save_path', type=str, default='./train.json',
-                    help="if not split the dataset, give a path to a json file")
-parser.add_argument('--random_split', action='store_true', help="random split the dataset, default ratio is 8:1:1")
 arg = parser.parse_args()
+
 
 def yolo2coco(root_path):
     originLabelsDir = os.path.join(root_path, 'labels')
-    with open(os.path.join(root_path, 'classes.txt')) as f:
-        classes = f.read().strip().split()
-    # images dir name
-    indexes = os.listdir(originLabelsDir)
-    print('classes',classes)
-    print('indexes',indexes)
-
-    # dataset = {'categories': [], 'annotations': [], 'images': []}
-    # for i, cls in enumerate(classes, 0):
-    #     dataset['categories'].append({'id': i, 'name': cls, 'supercategory': 'mark'})
+    with open(os.path.join(originLabelsDir, 'classes.txt')) as f:
+        classes = f.read().strip().split() # 获取类别
+    print('originLabelsDir', originLabelsDir)
+    # 图片文件夹名字
+    indexes = os.listdir(originLabelsDir)[1:]  # 排除 classes.txt 文件
+    # print('classes', classes)
+    # print('indexes', indexes)
 
     # 标注的id
     ann_id_cnt = 0
+    wl = 0  # width long
+    wh = 0  # width height
+    hl = 0  # height long
+    new_class = 0  # 分类
     for k, index in enumerate(tqdm(indexes)):
         # 支持 png jpg 格式的图片。
         txtFile = index.replace('images', 'txt').replace('.jpg', '.txt').replace('.png', '.txt')
+        file_data = ""
         with open(os.path.join(originLabelsDir, txtFile), 'r') as fr:
             labelList = fr.readlines()
-            print(index,len(labelList))
-            small = 0
-            middle = 0
-            large = 0
+            # print(index, len(labelList)) # 输出文件名，标注数
             for label in labelList:
-                label = label.strip().split()
-                # x = float(label[1])
-                # y = float(label[2])
-                w = float(label[3])
-                h = float(label[4])
-
-
-                # print('w',w)
-                # print('h',h)
+                labels = label.strip().split()
+                old_class = int(labels[0])
+                # x = float(labels[1])
+                # y = float(labels[2])
+                w = float(labels[3])
+                h = float(labels[4])
+                ratio = w / h
+                if ratio > 2:
+                    wl += 1
+                    new_class = 2
+                elif ratio < 0.5:
+                    hl += 1
+                    new_class = 0
+                else:
+                    wh += 1
+                    new_class = 1
+                label = label.replace(str(old_class) + ' ', str(new_class) + ' ')
+                # print('label', label)
                 ann_id_cnt += 1
-            print('small',small,'middle',middle,'large',large)
+                file_data += label
+        with open(os.path.join(originLabelsDir, txtFile), 'w') as fr:
+            fr.write(file_data)
+    print('总类别数')
+    print('wl宽长', wl, 'wh中等', wh, 'hl高长', hl)
     return
+
     # 保存结果
-    # folder = os.path.join(root_path, 'annotations')
+    # folder = os.path.join(root_path, 'class3')
     # if not os.path.exists(folder):
     #     os.makedirs(folder)
-    # json_name = os.path.join(root_path, 'annotations/{}'.format(arg.save_path))
+    # json_name = os.path.join(root_path, 'class3/{}'.format(arg.save_path))
     # with open(json_name, 'w') as f:
     #     json.dump(dataset, f)
     #     print('Save annotation to {}'.format(json_name))
-
 
 
 if __name__ == "__main__":
